1537947e5605e124db8ba22172180da0a6bd5939
chonghuimin <61720578+chonghuimin@users.noreply.github.com>
2020-03-25 15:56:08 +0800

    added dashboard

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,8 @@
              <li><router-link to="/" exact>Home</router-link></li>
              <li><router-link to="/add" exact>Add Item</router-link></li>
              <li><router-link to="/list" exact>List</router-link></li>
+             <li><router-link to="/visuals" exact>Analytics</router-link></li>
+
           </ul>
         </nav>
   </div>
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,11 @@
 import PageContent from './components/PageContent.vue'
 import AddItem from './components/AddItem.vue'
 import Home from './components/Home.vue'
+//added
+import Analytics from './components/Analytics.vue'
 export default[
     { path: '/', component: Home},
     { path: '/add', component: AddItem},
-    { path: '/list', component: PageContent}
+    { path: '/list', component: PageContent},
+    { path: '/visuals', component: Analytics}
 ]

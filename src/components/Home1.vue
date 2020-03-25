c291b448e229db5f3187ddb2cb6b170316a3166a
deniseannn <61720330+deniseannn@users.noreply.github.com>
2020-03-25 15:37:48 +0800

    login page

---
+++ src/components/Home1.vue
@@ -0,0 +1,82 @@
+<template>
+    <body bgcolor = "#fff3e6">
+	<div class = "container">
+        <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <img src = "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt = "Discussion" style = "width: 100%;">
+		<div class = "content">
+			<h1>Hello! <br> Welcome to GroupFinder! Click to join a group now.</h1>
+			<button value = "Find Group" onclick="document.location = 'grpfinder.html'"><i class="fa fa-group"></i> Find Group</button>
+			<button value = "Create Group" id = "continue" onclick="document.location = 'creategroup.html'"><i class="fa fa-pencil-square-o"></i> Create Group</button>
+			<button value = "About Us" id = "continue" onclick="document.location = 'aboutus.html'"><i class="fa fa-info-circle"></i> About Us</button>
+		</div>
+	</div>
+    </body>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+* {
+    box-sizing: border-box;
+} 
+
+
+body {
+    position: fixed;
+    margin: 0;
+}
+
+
+.container {
+    position: fixed;
+    margin: 0 auto;
+    height: 80%;
+    width: 100%;
+}
+
+.container img {
+	margin-bottom: -5px;
+}
+
+.container .content {
+    position: absolute;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5); /* Black background with transparency */
+    color: #f1f1f1;
+    width: 100%;
+    padding: 20px;
+}
+
+
+button {
+	font-size: large;
+	border-radius: 4px;
+	border: 3px solid #ff91af;
+	padding: 10px;
+    cursor: pointer;
+    background-color: #ff91af;
+    color: white;
+}
+
+button:hover {
+    background-color: white;
+    color: black;
+    border: 3px solid #ff91af;
+}
+
+#continue {
+	margin-left: 1em;
+}
+
+.credits {
+	position: absolute;
+    bottom: 6px;
+    right: 10px;
+    color: white;
+    font-size: 8px;
+}
+
+</style>
---
+++ src/components/Home2.vue
@@ -0,0 +1,148 @@
+<template>
+<body>
+  <div class="main">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    
+    <p class="sign">Login with NUS</p>
+        <form name="login" @submit.prevent="validate">
+        <input class= "email" name="email" type="text" align="center" placeholder="NUS email" v-model="email">
+        <input class= "pass" name="pass" type="text" align="center" placeholder="Password" v-model="pass">
+        <button value="Submit" type="submit" >Submit</button>
+        </form>
+                
+    </div>
+     
+</body>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            name:"",
+            pass:"",
+            Allusers: [{name: "Sam", email:"e0525169@u.nus.edu",password:"1234"},
+            {name: "Tom", email:"e0876234@u.nus.edu",password:"hello"},
+            {name: "Jess", email:"e0876234@u.nus.edu",password:"hello453"}] 
+        }
+    },
+    methods: {
+        validate: function() {
+            var name = document.forms["login"]["email"].value;
+            var pass = document.forms["login"]["pass"].value;
+            if (name == "") {
+                alert("Please enter your NUS email.");
+                return false;
+            } else if (pass == "") {
+                alert("Please enter your password.");
+                return false;
+            } 
+
+            var existUserName = false;
+            var existPassword = false;
+            var userName = "" 
+            for (const i of this.Allusers) {
+                if (i.email == name) {
+                    existUserName = true;
+                    if (i.password == pass) {
+                    existPassword = true;
+                    userName = i.name;
+                    } 
+                } 
+            }
+
+            if (existPassword && existUserName) {
+                alert("Welcome Back, " + userName + " !");
+            } else if (!existUserName) {
+                alert("User does not exist.");
+            } else if (!existPassword && existUserName) {
+                alert("Incorrect password.");
+            }
+            
+        }   
+    }
+}
+</script>>
+
+<style scoped>
+
+.main {
+    background-color: #FFFFFF;
+    width: 500px;        
+    height: 300px;
+    margin: 6em auto;
+    border-radius: 1.5em;
+    box-shadow: 0px 11px 35px 2px rgba(0, 0, 0, 0.14);
+}
+    
+.sign {
+    padding-top: 40px;
+    color: #16A085;
+    font-family: 'Ubuntu', sans-serif;
+    font-weight: bold;
+    font-size: 23px;
+    margin-right: 35%;
+}
+
+.email {
+    width: 76%;
+    color: rgb(38, 50, 56);
+    font-weight: 700;
+    font-size: 14px;
+    letter-spacing: 1px;
+    background: rgba(136, 126, 126, 0.04);
+    padding: 10px 20px;
+    border-radius: 20px;
+    box-sizing: border-box;
+    border: 2px solid rgba(0, 0, 0, 0.02);
+    margin-bottom: 50px;
+    margin-left: 20px;
+    text-align: center;
+    margin-bottom: 27px;
+    font-family: 'Ubuntu', sans-serif;
+}
+    
+form.form1 {
+    padding-top: 40px;
+}
+    
+    .pass {
+    width: 76%;
+    color: rgb(38, 50, 56);
+    font-weight: 700;
+    font-size: 14px;
+    letter-spacing: 1px;
+    background: rgba(136, 126, 126, 0.04);
+    padding: 10px 20px;
+    border: none;
+    border-radius: 20px;
+    outline: none;
+    box-sizing: border-box;
+    border: 2px solid rgba(0, 0, 0, 0.02);
+    margin-bottom: 50px;
+    margin-left: 20px;
+    text-align: center;
+    margin-bottom: 27px;
+    font-family: 'Ubuntu', sans-serif;
+    }
+  
+    
+button {
+    cursor: pointer;
+    border-radius: 5em;
+    color: #fff;
+    background: #16A085;
+    border: 0;
+    padding-left: 40px;
+    padding-right: 40px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 13px;
+    margin-left:55%; 
+    }
+
+</style>
